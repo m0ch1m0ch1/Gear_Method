12d612bccc39bbf8fbd0b64776f0e52a4fa681fa
Motomura Yohei <y.motomura0426@gmail.com>
2015-11-10 12:54:56 +0900

    プログラムの動作確認

--- bin/Sample2/diff.c
+++ bin/Sample2/diff.c
@@ -1,20 +1,22 @@
+/*
+ diff.c : Function for definition of differential equation
+*/
+
 #include "common.h"
 
 #define PI M_PI
 
 /* ------------------ Definition of differential equation ------------------ */
 
-void Differential_equation()
-{
+void Differential_equation(){
+
   int i;
 
-  for(i=0;i<number_x;i++)
-    {
-      x[i] = y[0][i];
-    }
+  for( i = 0 ; i < number_x ; i++){
+    x[i] = y[0][i];
+  }
 
-  df[0] = 1.0;
-
+  df[0] = 1.000000000000000E-00;
   df[1] = k[1]*x[3] + k[4]*x[4] - k[0]*x[1]*x[2];
   df[2] = k[1]*x[3] - k[0]*x[1]*x[2];
   df[3] = k[0]*x[1]*x[2] + k[3]*x[4] - k[1]*x[3] - k[2]*x[3];
