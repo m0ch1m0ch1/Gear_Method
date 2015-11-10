a513db9b9093f875ae299f7af6f73abcfac35c01
Motomura Yohei <y.motomura0426@gmail.com>
2015-11-10 19:40:01 +0900

    グラフ描写プログラムの修正

--- src/graph.c
+++ src/graph.c
@@ -15,16 +15,20 @@ void Graph(){
   fprintf(gp, "set datafile separator \",\"\n");
 
   sscanf(filename[1],"%[^.]]",buffer);
-
   fprintf(gp, "set output \"./%s.jpeg\"\n",buffer);
+
   fprintf(gp, "plot");
 
   for( i = 1 ; i < number_x ; i++){
-    fprintf(gp, "\"./%s\" using 1:%d title \"%s\" with l",filename[1], i+1, name_x[i]);
-    if (i != (number_x-1)){fprintf(gp,",");}
+
+    fprintf(gp, "\"./%s\" using 1:%d title \"%s\" w lp",filename[1], i+1, name_x[i]);
+    if (i != ( number_x - 1 )){
+      fprintf(gp,",\\");
+    }
+    fprintf(gp,"\n");
+
   }
 
-  fprintf(gp,"\n");
   fflush(gp);
   pclose(gp);
 
